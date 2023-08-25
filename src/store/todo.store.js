5c05b34f56cb5e634e54130209c90be8b57e19b2
Rafael Ara <rafael.ara.casanova@gmail.com>
2023-08-25 02:07:36 -0600

    add Methods Store

--- src/store/todo.store.js
+++ src/store/todo.store.js
@@ -20,6 +20,48 @@ const initStore = () => {
     console.log('InitStore 🥑');
 }
 
+const loadStore = () => {
+    throw new Error('Not implemented');
+    
+}
+
+/**
+ * 
+ * @param {String} description 
+ */
+const addTodo = (description) => {
+    throw new Error('Not implemented');
+}
+/**
+ * 
+ * @param {String} todoId Todo indentified
+ */
+const toggleTodo = ( todoId ) =>{
+    throw new Error('Not implemented');
+}
+
+const deleteTodo = ( todoId ) => {
+    throw new Error('Not implemented');
+} 
+
+const deleteCompleted = (  ) => {
+    throw new Error('Not implemented');
+}
+
+const setFilter = ( newFilter = Filters.All) => {
+    throw new Error('Not implemented');
+}
+
+const getCurrentFilter = () => {
+    throw new Error('Not implemented');
+}
+
 export default {
+    deleteCompleted,
+    deleteTodo,
+    getCurrentFilter,
     initStore,
+    loadStore,
+    setFilter,
+    toggleTodo,
 }
