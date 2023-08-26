9aa88bd0bca75d3eeaf009eccf94b306150abe37
Rafael Ara <rafael.ara.casanova@gmail.com>
2023-08-26 03:08:30 -0600

    todo.store add code

--- src/store/todo.store.js
+++ src/store/todo.store.js
@@ -7,7 +7,7 @@ const Filters = {
 }
 
 const state = {
-    todo: [
+    todos: [
         new Todo('Piedra del alma'),
         new Todo('Piedra del infinito'),
         new Todo('Piedra del tiempo'),
@@ -24,42 +24,65 @@ const loadStore = () => {
     throw new Error('Not implemented');
     
 }
+const getTodos = ( filter = Filters.All) => {
+    switch (filter) {
+        case Filters.All:
+            return [...state.todos];
+        case Filters.Completed:
+            return state.todos.filter( todo => todo.done);
+        case Filters.Pending:
+            return state.todos.filter( todo => !todo.done);
+        default:
+            throw new Error(`Option ${filter} is not valid`)
+    }
+}
 
 /**
- * 
- * @param {String} description 
+ * Add new Task
+ * @param {String} description  Receive the new task to add
  */
 const addTodo = (description) => {
-    throw new Error('Not implemented');
+    if (!description) throw new Error(`Description is required`);
+    state.todos.push(new Todo(description));
 }
 /**
  * 
- * @param {String} todoId Todo indentified
+ * @param {String} todoId TODO indentified
  */
 const toggleTodo = ( todoId ) =>{
     throw new Error('Not implemented');
 }
-
+/**
+ * 
+ * @param {String} todoId TODO indentified
+ */
 const deleteTodo = ( todoId ) => {
-    throw new Error('Not implemented');
+    state.todos = state.todos.filter(todo => todo.id !== todoId);
+    
 } 
 
-const deleteCompleted = (  ) => {
-    throw new Error('Not implemented');
-}
 
+const deleteCompleted = (  ) => {
+    state.todos = state.todos.filter(todo => todo.done);
+}
+/**
+ * 
+ * @param {Filters} newFilter 
+ */
 const setFilter = ( newFilter = Filters.All) => {
-    throw new Error('Not implemented');
+    state.filter = newFilter;
 }
 
 const getCurrentFilter = () => {
-    throw new Error('Not implemented');
+    return state.filter;
 }
 
 export default {
+    addTodo,
     deleteCompleted,
     deleteTodo,
     getCurrentFilter,
+    getTodos,
     initStore,
     loadStore,
     setFilter,
